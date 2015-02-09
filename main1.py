c6299c171219743b8a22fec29e240c776175f193
Mauro Mizrahi <mauro.mizrahi@etu.upmc.fr>
2015-02-09 17:24:00 +0100

    mise à jour main

---
+++ main1.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 26 19:32:52 2015
+
+@author: 3301031
+"""
+
+
+from soccersimulator import SoccerBattle, SoccerTeam, SoccerPlayer
+from classes import *
+
+team1=SoccerTeam("BocaJuniors")
+team2=SoccerTeam("ArgentinaFC")
+team1.add_player(SoccerPlayer("t1j1",JoueurFonceur()))
+team1.add_player(SoccerPlayer("t1j2",RandomStrategy()))
+team2.add_player(SoccerPlayer("t2j1",ComposeStrategy(DefenseurContreAttaque(), JoueurFonceur())))
+team2.add_player(SoccerPlayer("t2j2",RandomStrategy()))
+team2.add_player(SoccerPlayer("t2j3",GoalContreAttaque()))
+team2.add_player(SoccerPlayer("t2j4",DefenseurContreAttaque()))
+
+teams = [BocaJuniors, ArgentinaFC]
+names = "MAUFC"
+
