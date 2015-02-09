38f0bba395624dad620fc76d49e35f3c3893f348
Mauro Mizrahi <mauro.mizrahi@etu.upmc.fr>
2015-02-09 16:51:13 +0100

    Nom d'équipes changés

--- main.py
+++ main.py
@@ -9,12 +9,14 @@ Created on Mon Jan 26 19:32:52 2015
 from soccersimulator import SoccerBattle, SoccerTeam, SoccerPlayer
 from classes import *
 
-team1=SoccerTeam("team1")
+team1=SoccerTeam("BocaJuniors")
 team2=SoccerTeam("team2")
-team1.add_player(SoccerPlayer("t1j1",JoueurFonceur()))
-team2.add_player(SoccerPlayer("t2j1",ComposeStrategy(DefenseurContreAtaque(), JoueurFonceur())))
-team1.add_player(SoccerPlayer("t1j2",RandomStrategy()))
+BocaJuniors.add_player(SoccerPlayer("t1j1",JoueurFonceur()))
+BocaJuniors.add_player(SoccerPlayer("t1j2",RandomStrategy()))
+team2.add_player(SoccerPlayer("t2j1",ComposeStrategy(DefenseurContreAttaque(), JoueurFonceur())))
 team2.add_player(SoccerPlayer("t2j2",RandomStrategy()))
+team2.add_player(SoccerPlayer("t2j3",GoalContreAttaque()))
+team2.add_player(SoccerPlayer("t2j3",DefenseurContreAttaque()))
 
 teams = [team1, team2]
-names = "BocaJuniors"
+names = "MAUFC"
